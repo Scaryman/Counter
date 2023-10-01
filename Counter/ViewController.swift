bafe5421dee4868df0c0a9715a642ea16467c437
Scaryman <scaryman86@gmail.com>
2023-10-01 20:36:47 +0300

    Разработка приложения согласно заданию третьего спринта

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    private var counter: Int = 0 {
+        didSet {
+            updateCounterLabel()
+        }
+    }
+    
+    private func updateCounterLabel() {
+        counterLabel.text = "\(counter)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        clearHistory()
     }
 
-
+    @IBAction func plusDidTap() {
+        counter += 1
+        addMessageToHistory(message: "значение изменено на +1")
+    }
+    
+    @IBAction func minusDidTap() {
+        if counter > 0 {
+            counter -= 1
+            addMessageToHistory(message: "значение изменено на -1")
+        } else {
+            addMessageToHistory(message: "попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction func resetDidTap() {
+        counter = 0
+        addMessageToHistory(message: "значение сброшено")
+    }
+    
+    private func addMessageToHistory(message: String) {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "dd.MM.YYYY HH:mm:ss"
+        
+        let dateString = dateFormatter.string(from: currentDate)
+        
+        historyTextView.text.append("[\(dateString)]: \(message)\n")
+        
+        let range = NSMakeRange(historyTextView.text.count - 1, 0)
+        historyTextView.scrollRangeToVisible(range)
+    }
+    
+    @IBAction func clearHistoryDidTap() {
+        clearHistory()
+    }
+    
+    private func clearHistory() {
+        historyTextView.text = "История изменений:\n"
+    }
 }
 
